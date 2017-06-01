6ce5c7e12ad55ed36254fb3a8d3571bd74ec8833
Matthew Manning <matt.manning@gmail.com>
2017-06-01 15:50:14 -0400

    service environment

--- manifest/yaml.go
+++ manifest/yaml.go
@@ -139,6 +139,8 @@ func (v Services) MarshalYAML() (interface{}, error) {
 		command["production"] = s.Command.Production
 		service["command"] = command
 
+		service["environment"] = s.Environment
+
 		services[s.Name] = service
 	}
 
